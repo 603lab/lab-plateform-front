79ae1f8cf616897c1cbcee5d59f77f4c240eff64
Isaac <IsaacLu@ssw.com.au>
2019-03-04 16:19:14 +0800

    转移json文件以及部分优化

--- src/pages/Home/ArticleCard.jsx
+++ src/pages/Home/ArticleCard.jsx
@@ -1,225 +1,9 @@
 import React from 'react';
 import { Card, Tabs, Avatar, Button, Pagination, List } from 'antd';
 import styles from './ArticleCard.less';
+import { developerData, articleData, techDirectionsType } from './ArticleCardJson';
 
 const { TabPane } = Tabs;
-const developerData = [
-  {
-    id: '1',
-    realName: '陈晓斌',
-    avatar: '',
-    className: '15广播电视工程2班',
-    techDirection: '前端',
-    followedNum: 42,
-    tag: 'React;前端;大佬',
-    isFollowed: true,
-  },
-  {
-    id: '2',
-    realName: '大白菜',
-    avatar: '',
-    className: '16广播电视工程2班',
-    techDirection: '前端',
-    followedNum: 1,
-    tag: 'React;前端;菜鸡',
-    isFollowed: true,
-  },
-  {
-    id: '3',
-    realName: '沈江峰',
-    avatar: '',
-    className: '15广播电视工程2班',
-    techDirection: 'IOS',
-    followedNum: 32,
-    tag: '脱衣狂魔;小鸡短裤',
-    isFollowed: true,
-  },
-  {
-    id: '4',
-    realName: '你傲哥',
-    avatar: '',
-    className: '15广播电视工程2班',
-    techDirection: '全栈',
-    followedNum: 8,
-    tag: 'React;前端;大佬',
-    isFollowed: false,
-  },
-  {
-    id: '5',
-    realName: '赵日天',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '后端',
-    followedNum: 8,
-    tag: 'C#;.NET;话痨',
-    isFollowed: false,
-  },
-  {
-    id: '6',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bbbbbbbbbbbbbbbbb..',
-    isFollowed: false,
-  },
-  {
-    id: '7',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-  {
-    id: '8',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-  {
-    id: '9',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-  {
-    id: '10',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-  {
-    id: '11',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-  {
-    id: '12',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-  {
-    id: '13',
-    realName: '用户',
-    avatar: '',
-    className: '17电科1班',
-    techDirection: '产品',
-    followedNum: 23,
-    tag: 'C#;aaa;aaaa;bb',
-    isFollowed: false,
-  },
-];
-const articleData = [
-  {
-    id: '1',
-    fileAddress: '/Annex/doc/React总结（1）.doc',
-    fileName: 'React总结（1）',
-    fileTag: 'React;个人总结;前端',
-    url: null,
-    content: '简单的聊一聊关于React的一些总结和看法',
-    likeNum: 1,
-    collectNum: 0,
-    browseNum: 3,
-    commentNum: 0,
-    type: 'doc',
-    remark: '',
-    createUserName: '陈晓斌',
-    createTime: '2019-01-14T17:46:28',
-  },
-  {
-    id: '2',
-    fileAddress: '/Annex/doc/React总结（2）.doc',
-    fileName: 'React总结 -（2）',
-    fileTag: 'React;前端',
-    url: null,
-    content: '今天我们来说一说React的艺术，希望对大家有帮助',
-    likeNum: 1,
-    collectNum: 0,
-    browseNum: 3,
-    commentNum: 0,
-    type: 'doc',
-    remark: '',
-    createUserName: '陈晓斌',
-    createTime: '2019-01-04T19:46:28',
-  },
-  {
-    id: '3',
-    fileAddress: '/Annex/doc/实习全书.doc',
-    fileName: '如何正确的划水',
-    fileTag: '划水;666;走位',
-    url: null,
-    content: '实习所有人面临的大问题，至今都让人不解，如何正确XXX',
-    likeNum: 1,
-    collectNum: 0,
-    browseNum: 3,
-    commentNum: 0,
-    type: 'doc',
-    remark: '',
-    createUserName: '陆仁杰',
-    createTime: '2019-02-24T15:26:28',
-  },
-];
-
-const techDirectionsType = [
-  {
-    value: '前端',
-    key: '1',
-  },
-  {
-    value: '后端',
-    key: '2',
-  },
-  {
-    value: 'IOS',
-    key: '3',
-  },
-  {
-    value: 'Andriod',
-    key: '4',
-  },
-  {
-    value: '美工',
-    key: '5',
-  },
-  {
-    value: '产品',
-    key: '6',
-  },
-  {
-    value: 'AI智能',
-    key: '1',
-  },
-  {
-    value: 'VR世界',
-    key: '7',
-  },
-];
 
 const totalD = Object.keys(developerData).length;
 // const totalA = Object.keys(articleData).length;
@@ -261,28 +45,30 @@ export default class CommisionWork extends React.Component {
     const { currentPage, totalDeveloper } = this.state;
     return (
       <Tabs type="card" style={{ marginTop: 10 }}>
-        <TabPane tab="推荐大佬" key="tab-1" style={{ marginTop: -16, backgroundColor: 'white' }}>
+        <TabPane tab="推荐大佬" key="tabPhone" style={{ marginTop: -16, backgroundColor: 'white' }}>
           <Tabs style={{ height: 310 }}>
             {techDirectionsType.map(tech => (
               // 这里在做一个只有位置为当前页面才开始渲染
-              <TabPane tab={tech.value} key={`tabPhone${tech.key}`} style={{ padding: 10 }}>
+              <TabPane tab={tech.value} key={`childTabPhone${tech.key}`} style={{ padding: 10 }}>
                 <Card
                   key={`tabCard${tech.key}`}
                   style={{ width: '100%', marginTop: -20, height: 228 }}
                 >
                   {developerData.map((item, i) => (
-                    <>
+                    <div key={`mainDiv${item.id}`}>
                       {this.isMap(i) ? (
-                        <Card.Grid style={gridStyle} key={`tabPhone${tech.key}${item.id}`}>
+                        <Card.Grid style={gridStyle} key={`cardGrid${tech.key}${item.id}`}>
                           <div className={styles.avatarInfo}>
                             <Avatar
                               style={{ height: 50, width: 50 }}
                               src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                              key={`avatar${item.id}`}
                             />
                             <Button
                               type="primary"
                               size="small"
                               style={{ marginTop: 5, width: 58, marginLeft: -1 }}
+                              key={`follow${item.id}`}
                             >
                               {item.isFollowed === true ? '已关注' : '关注'}
                             </Button>
@@ -295,7 +81,7 @@ export default class CommisionWork extends React.Component {
                           </div>
                         </Card.Grid>
                       ) : null}
-                    </>
+                    </div>
                   ))}
                 </Card>
                 <Pagination
--- src/pages/Home/CommisionWork.jsx
+++ src/pages/Home/CommisionWork.jsx
@@ -2,58 +2,14 @@ import React from 'react';
 import { Card, Icon, Alert, Button } from 'antd';
 import moment from 'moment';
 import styles from './CommisionWork.less';
+import {
+  cardData,
+  cardStyle,
+  cardDefaultStyle,
+  cardMoveLeftStyle,
+  cardMoveRightStyle,
+} from './CommisionWorkJson';
 
-const cardData = [
-  {
-    id: '1',
-    taskTitle: '一篇博客',
-    endTime: '2019-01-18T00:00:00',
-    lastEditTime: '2019-01-11T17:53:35',
-    taskDescription: '本周轮到你总结博客，主题自定义哦',
-  },
-  {
-    id: '2',
-    taskTitle: '本周是你的分享会',
-    endTime: '2019-05-18T00:00:00',
-    lastEditTime: '2019-01-11T04:26:16',
-    taskDescription: '大家好我今天来介绍一下Mock的基本原理',
-  },
-  {
-    id: '3',
-    taskTitle: '电商项目汇总',
-    endTime: '2019-05-28T00:00:00',
-    lastEditTime: '2019-03-02T14:53:35',
-    taskDescription: '帮助老师整理电商名单',
-  },
-  {
-    id: '4',
-    taskTitle: '艺考生！冲鸭！',
-    endTime: '2019-01-29T00:00:00',
-    lastEditTime: '2019-03-01T14:53:35',
-    taskDescription: '请协助完成艺考任务',
-  },
-  {
-    id: '5',
-    taskTitle: '上课提醒',
-    endTime: '2019-04-12T00:00:00',
-    lastEditTime: '2019-02-11T17:53:35',
-    taskDescription: '别旷课，别迟到',
-  },
-  {
-    id: '6',
-    taskTitle: '小斌 关注了你',
-    endTime: '2019-03-18T00:00:00',
-    lastEditTime: '2019-02-21T17:53:35',
-    taskDescription: '马上开始你的任务吧',
-  },
-  {
-    id: '7',
-    taskTitle: '天气预报',
-    endTime: '2019-02-18T00:00:00',
-    lastEditTime: '2019-02-27T17:53:35',
-    taskDescription: '30天连续暴雨，下的你怀疑人生',
-  },
-];
 // 卡片宽度
 const cardWidth = 274;
 // 设置卡片长度
@@ -62,31 +18,6 @@ const cardLength = Object.keys(cardData).length;
 const utMost = cardWidth * (cardLength - 1);
 // 定义单次点击以后移动的距离
 const moveWidth = 274;
-const cardStyle = {
-  marginTop: '-10px',
-  width: '300px',
-  height: '180px',
-  float: 'left',
-  zIndex: 1,
-};
-const cardDefaultStyle = {
-  height: '180px',
-  width: '274px',
-  float: 'left',
-  marginLeft: '0px',
-};
-const cardMoveLeftStyle = {
-  height: '180px',
-  marginTop: '5px',
-  width: '274px',
-  float: 'left',
-};
-const cardMoveRightStyle = {
-  height: '180px',
-  marginTop: '5px',
-  width: '274px',
-  float: 'left',
-};
 
 export default class CommisionWork extends React.Component {
   constructor(args) {
