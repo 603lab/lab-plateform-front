40fc3302bdd145a11d358d556413dce02af07502
chenxiaobin <chenxiaobin@youzan.com>
2019-03-29 17:17:05 +0800

    [fix]去除console.log

--- src/services/account.js
+++ src/services/account.js
@@ -2,7 +2,7 @@
  * @Author: chenxiaobin
  * @Date: 2019-03-28 21:26:41
  * @Last Modified by: chenxiaobin
- * @Last Modified time: 2019-03-29 14:29:42
+ * @Last Modified time: 2019-03-29 17:14:57
  */
 
 import request from '@/utils/request';
@@ -15,7 +15,6 @@ const basicPrefix = 'Base-Module/Users';
  * @param {string} password	 	登陆用户密码	required
  */
 export async function fakeAccountLogin(params) {
-  console.log('params', params);
   return request(`/api/${basicPrefix}/Login`, {
     method: 'POST',
     body: {
