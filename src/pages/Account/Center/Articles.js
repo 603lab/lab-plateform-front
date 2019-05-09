ac06bb0ee7dd185e4bd2b5c68c50b361b34846a6
chenxiaobin <chenxiaobin@youzan.com>
2019-05-09 15:50:35 +0800

    [fix] 1.抽离点赞动画逻辑 2. 完善点赞动画逻辑

--- src/pages/Account/Center/Articles.js
+++ src/pages/Account/Center/Articles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { List, Icon, Tag } from 'antd';
-import mojs from 'mo-js';
 import { connect } from 'dva';
+import { likeAnimation } from '@/utils/animation';
 import ArticleListContent from '@/components/ArticleListContent';
 // import Store from '@/utils/store';
 import styles from './Articles.less';
@@ -25,104 +25,22 @@ class CenterArticle extends React.Component {
     };
   }
 
-  showAnimation = index => {
-    const LikeWrapper = document.getElementsByClassName('like-wrapper')[index];
-    const LikeIcon = document.getElementsByClassName('like-icon')[index];
-    const opacityCurve16 = mojs.easing.path('M0,0 L25.333,0 L75.333,100 L100,0');
-    const translationCurve16 = mojs.easing.path(
-      'M0,100h25.3c0,0,6.5-37.3,15-56c12.3-27,35-44,35-44v150c0,0-1.1-12.2,9.7-33.3c9.7-19,15-22.9,15-22.9'
-    );
-    const squashCurve16 = mojs.easing.path(
-      'M0,100.004963 C0,100.004963 25,147.596355 25,100.004961 C25,70.7741867 32.2461944,85.3230873 58.484375,94.8579105 C68.9280825,98.6531013 83.2611815,99.9999999 100,100'
-    );
-    const Burst = new mojs.Burst({
-      parent: LikeWrapper,
-      duration: 1700,
-      delay: 350,
-      shape: 'circle',
-      fill: '#722ed1',
-      x: '40%',
-      y: '50%',
-      opacity: 0.3,
-      childOptions: { radius: { 'rand(15,5)': 0 } },
-      radius: { 0: 150 },
-      degree: 50,
-      angle: -25,
-      count: 6,
-      isRunLess: true,
-      easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
-    });
-    // burst animation (line1)
-    const Burst2 = new mojs.Burst({
-      parent: LikeWrapper,
-      duration: 600,
-      delay: 200,
-      shape: 'circle',
-      fill: '#C0C1C3',
-      x: '15%',
-      y: '100%',
-      childOptions: {
-        radius: { 60: 0 },
-        type: 'line',
-        stroke: '#722ed1',
-        strokeWidth: 2,
-        strokeLinecap: 'round',
-      },
-      radius: { 50: 180 },
-      angle: 180,
-      count: 1,
-      opacity: 0.4,
-      isRunLess: true,
-      easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
-    });
-    // burst animation (line2)
-    const Burst3 = new mojs.Burst({
-      parent: LikeWrapper,
-      duration: 600,
-      delay: 200,
-      shape: 'circle',
-      fill: '#722ed1',
-      x: '50%',
-      y: '100%',
-      childOptions: {
-        radius: { 60: 0 },
-        type: 'line',
-        stroke: '#722ed1',
-        strokeWidth: 2,
-        strokeLinecap: 'round',
-      },
-      radius: { 50: 220 },
-      angle: 180,
-      count: 1,
-      opacity: 0.4,
-      isRunLess: true,
-      easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
-    });
-    const Tween = new mojs.Tween({
-      duration: 500,
-      onUpdate: progress => {
-        const translateProgress = translationCurve16(progress);
-        const squashProgress = squashCurve16(progress);
-        const scaleX = 1 - 2 * squashProgress;
-        const scaleY = 1 + 2 * squashProgress;
+  showAnimation = (isLike, index) => {
+    /**
+     * @arguments {0}  父级DOM
+     * @arguments {1}  子级DOM
+     * @arguments {2}  当前操作对象
+     * @arguments {3}  动画类型 false 简约型(取消点赞) true 饱满型(点赞)
+     */
 
-        const t = `translate3d(0, ${-180 * translateProgress}px,0) scale3d(${scaleX},${scaleY},1)`;
-        const opacityProgress = opacityCurve16(progress);
-        LikeIcon.style.transform = t;
-        LikeIcon.style.WebkitTransform = t;
-        LikeIcon.style.opacity = opacityProgress;
-      },
-    });
-    index === 0
-      ? new mojs.Timeline().add(Tween).play()
-      : new mojs.Timeline().add(Burst, Burst2, Burst3, Tween).play();
+    likeAnimation('like-wrapper', 'like-icon', index, isLike);
   };
 
   handleArticleAction = (info, index) => {
     const { dispatch } = this.props;
     const { id, isLike } = info;
     // const userInfo = Store.getBasicInfo();
-    this.showAnimation(index);
+    this.showAnimation(isLike, index);
     dispatch({
       type: 'list/like',
       payload: {
---
+++ src/utils/animation.js
@@ -0,0 +1,102 @@
+import mojs from 'mo-js';
+
+// 点赞动画
+/**
+ * @arguments {0}  父级DOM
+ * @arguments {1}  子级DOM
+ * @arguments {2}  当前操作对象
+ * @arguments {3}  当前是否点赞决定动画类型 true 简约型(已点赞,需要取消点赞) false 饱满型(点赞)
+ */
+
+export function likeAnimation(parentClass, selfClass, index, type) {
+  const LikeWrapper = document.getElementsByClassName(parentClass)[index];
+  const LikeIcon = document.getElementsByClassName(selfClass)[index];
+  const opacityCurve16 = mojs.easing.path('M0,0 L25.333,0 L75.333,100 L100,0');
+  const translationCurve16 = mojs.easing.path(
+    'M0,100h25.3c0,0,6.5-37.3,15-56c12.3-27,35-44,35-44v150c0,0-1.1-12.2,9.7-33.3c9.7-19,15-22.9,15-22.9'
+  );
+  const squashCurve16 = mojs.easing.path(
+    'M0,100.004963 C0,100.004963 25,147.596355 25,100.004961 C25,70.7741867 32.2461944,85.3230873 58.484375,94.8579105 C68.9280825,98.6531013 83.2611815,99.9999999 100,100'
+  );
+  const Burst = new mojs.Burst({
+    parent: LikeWrapper,
+    duration: 1700,
+    delay: 350,
+    shape: 'circle',
+    fill: '#722ed1',
+    x: '40%',
+    y: '50%',
+    opacity: 0.3,
+    childOptions: { radius: { 'rand(15,5)': 0 } },
+    radius: { 0: 150 },
+    degree: 50,
+    angle: -25,
+    count: 6,
+    isRunLess: true,
+    easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+  });
+  // burst animation (line1)
+  const Burst2 = new mojs.Burst({
+    parent: LikeWrapper,
+    duration: 600,
+    delay: 200,
+    shape: 'circle',
+    fill: '#C0C1C3',
+    x: '15%',
+    y: '100%',
+    childOptions: {
+      radius: { 60: 0 },
+      type: 'line',
+      stroke: '#722ed1',
+      strokeWidth: 2,
+      strokeLinecap: 'round',
+    },
+    radius: { 50: 180 },
+    angle: 180,
+    count: 1,
+    opacity: 0.4,
+    isRunLess: true,
+    easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+  });
+  // burst animation (line2)
+  const Burst3 = new mojs.Burst({
+    parent: LikeWrapper,
+    duration: 600,
+    delay: 200,
+    shape: 'circle',
+    fill: '#722ed1',
+    x: '50%',
+    y: '100%',
+    childOptions: {
+      radius: { 60: 0 },
+      type: 'line',
+      stroke: '#722ed1',
+      strokeWidth: 2,
+      strokeLinecap: 'round',
+    },
+    radius: { 50: 220 },
+    angle: 180,
+    count: 1,
+    opacity: 0.4,
+    isRunLess: true,
+    easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+  });
+  const Tween = new mojs.Tween({
+    duration: 500,
+    onUpdate: progress => {
+      const translateProgress = translationCurve16(progress);
+      const squashProgress = squashCurve16(progress);
+      const scaleX = 1 - 2 * squashProgress;
+      const scaleY = 1 + 2 * squashProgress;
+      const t = `translate3d(0, ${-180 * translateProgress}px,0) scale3d(${scaleX},${scaleY},1)`;
+      const opacityProgress = opacityCurve16(progress);
+      LikeIcon.style.transform = t;
+      LikeIcon.style.WebkitTransform = t;
+      LikeIcon.style.opacity = opacityProgress;
+    },
+  });
+  console.log('type', type);
+  type
+    ? new mojs.Timeline().add(Tween).play()
+    : new mojs.Timeline().add(Burst, Burst2, Burst3, Tween).play();
+}
